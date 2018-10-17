c4074bb4214788c8895acd16df625c8cf90deac3
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2018-10-17 09:46:14 -0700

    Create maps.js

---
+++ maps.js
@@ -0,0 +1,8 @@
+function Waterfall(Splash) {
+    var Dragonfly = Splash;
+    if (Dragonfly.style.display === "none") {
+        Dragonfly.style.display = "block";
+    } else {
+        Dragonfly.style.display = "none";
+    }
+}
