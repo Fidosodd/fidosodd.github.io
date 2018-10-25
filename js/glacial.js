0e804b40b42c8d3ca50b96999148aa9ca7db2756
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2018-10-25 14:03:27 -0700

    Update glacial.js

--- js/glacial.js
+++ js/glacial.js
@@ -1,6 +1,9 @@
 function glacial(x_coord, y_coord, frost) {
-    var x = x_coord;
-    var y = y_coord;
-    frost.left = x;
-    frost.top = y;
+    var elsa = x_coord;
+    var anna = y_coord;
+    var mercury = frost;
+    mercury.left = elsa;
+    mercury.top = anna;
+    //Thank you Lyle Altschul for these names.
+    //Though you can't draw Stalin, you'll always have a special place in our hearts.
 }
