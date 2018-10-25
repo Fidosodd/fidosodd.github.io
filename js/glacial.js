6fd06a368d0d887ef2ba5004343cbdc910b970f7
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2018-10-25 13:39:21 -0700

    Update and rename maps.js to glacial.js

---
+++ js/glacial.js
@@ -0,0 +1,6 @@
+function glacial(x_coord, y_coord) {
+    var x = x_coord;
+    var y = y_coord;
+    this.left = x;
+    this.right = x;
+}
--- js/maps.js
+++
@@ -1,7 +0,0 @@
-function location(x_coord, y_coord) {
-    var x = x_coord;
-    var y = y_coord;
-    var dot = this;
-    this.left = x;
-    dot.left = x;
-}
