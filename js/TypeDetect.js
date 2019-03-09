c023a1fc97b998717eadb0c8b70f8ed73edd5db5
Wide-Eyed Eel <wideeyedeel@gmail.com>
2019-03-08 19:05:02 -0800

    Update TypeDetect.js

--- js/TypeDetect.js
+++ js/TypeDetect.js
@@ -1,4 +1,4 @@
-functin typedetect() {
+function typedetect() {
     var isMobile = false; //initiate as false
     // device detection
     if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
@@ -6,3 +6,8 @@ functin typedetect() {
         isMobile = true;
     }
 }
+function deskchange() {
+    if (isMobile = true){
+        var menu = document.getElementById('menufull');
+        menufull.style.display = 'block';
+    }
